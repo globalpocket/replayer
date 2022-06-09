a3141b275a1cf3ffa1d40b82b6491dc7eaeaa08c
LEE <dev@globalpocket.jp>
2022-06-09 17:21:45 +0900

    Update index.js

--- index.js
+++ index.js
@@ -13,8 +13,10 @@ const dirNameByRecordings = './recordings'
 const dirNameByResult = './result'
 let dirNameByScreenshot = ''
 let dirNameByHttp = ''
+let dirCurrent = ''
 let countByStep = 0
 let countByResponse = 0
+let currentFlow = null
 const browser = await puppeteer.launch({
     args: ['--no-sandbox', '--disable-setuid-sandbox', '--lang=ja'],
     headless: false,
@@ -24,74 +26,138 @@ const page = await browser.newPage()
 page.on('response',async (response)=>{
     try{
         countByResponse++
-
         const url = await response.url()
-        console.error(`count:${countByResponse}:${url}`)
-        fs.writeFileSync(`${dirNameByHttp}/${countByResponse}.url.txt`, url)
-
-        const timing = await response.timing()
-        fs.writeFileSync(`${dirNameByHttp}/${countByResponse}.timing.txt`, JSON.stringify(timing))
-
-        const headers = await response.headers()
-        fs.writeFileSync(`${dirNameByHttp}/${countByResponse}.headers.txt`, JSON.stringify(headers))
-
+        console.info(`count:${countByResponse}:${url}`)
         const status = response.status()
         if(status == 200){
-            const ext = mime.extension(headers["content-type"])
-            const responseName = `${dirNameByHttp}/${countByResponse}.response.${ext}`
-            response.buffer().then((buffer)=>{
-                fs.writeFileSync(responseName, buffer)
-            }).catch((e)=>{
-                fs.writeFileSync(responseName, JSON.stringify(e))
-            })
+            let ext = mime.extension(response.headers["content-type"])
+            if(ext === "png"){
+                return
+            }
+            if(ext === "jpeg"){
+                return
+            }
+            if(ext === "jpg"){
+                return
+            }
+            if(ext === "gif"){
+                return
+            }
+            if(ext === "woff2"){
+                return
+            }
+            fs.appendFileSync(`${dirNameByHttp}/${countByResponse}.json`, JSON.stringify(await response.headers()))
         } else {
-            const responseName = `${dirNameByHttp}/${countByResponse}.response.txt`
-            fs.writeFileSync(responseName, `STATUS CODE : ${status}`)
+            fs.appendFileSync(`${dirNameByHttp}/${countByResponse}.httperror.json`, JSON.stringify(await response.headers()))
         }
     } catch(e) {
+        console.info("/----------------------------------")
+        console.info("--- error                       ---")
+        console.info("-----------------------------------")
         console.error(e)
-        fs.writeFileSync(`${dirNameByHttp}/${countByResponse}.error.txt`, JSON.stringify(e));
+        console.info("-----------------------------------")
+        console.info("----------------------------------/")
+        fs.appendFileSync(`${dirNameByHttp}/${countByResponse}.error.txt`, JSON.stringify(e));
     }
 })
-
+page.on('error', (e) => {
+    console.info("/----------------------------------")
+    console.info("--- error                       ---")
+    console.info("-----------------------------------")
+    console.error('error', e)
+    console.info("-----------------------------------")
+    console.info("----------------------------------/")
+    fs.appendFileSync(`${dirNameByHttp}/${countByResponse}.error.txt`, JSON.stringify(e));
+})
+page.on('pageerror', (e) => {
+    console.info("/----------------------------------")
+    console.info("--- pageerror                   ---")
+    console.info("-----------------------------------")
+    console.error('pageerror', e)
+    console.info("-----------------------------------")
+    console.info("----------------------------------/")
+    fs.appendFileSync(`${dirNameByHttp}/${countByResponse}.error.txt`, JSON.stringify(e));
+})
+page.on('console', msg => {
+    const type = msg.type();
+    if(type === 'error'){
+        console.info("/----------------------------------")
+        console.info("--- console                     ---")
+        console.info("-----------------------------------")
+        for (let i = 0; i < msg.args.length; ++i){
+            console.error(`${i}: ${msg.args[i]}`)
+        }
+        console.info("-----------------------------------")
+        console.info("----------------------------------/")
+    }
+})
 class Extension extends PuppeteerRunnerExtension {
 
     async beforeAllSteps(flow) {
+        currentFlow = flow
         await super.beforeAllSteps(flow)
-        console.log('starting')
-
+        console.info('starting',flow)
         countByStep = 0
         countByResponse = 0
-        dirNameByScreenshot = `${dirNameByResult}/${currentPlay}/screenshots`
+        dirCurrent = `${dirNameByResult}/${currentPlay}`
+        dirNameByScreenshot = `${dirCurrent}/screenshots`
         fs.mkdirsSync(dirNameByScreenshot)
-        dirNameByHttp = `${dirNameByResult}/${currentPlay}/http`
+        dirNameByHttp = `${dirCurrent}/http`
         fs.mkdirsSync(dirNameByHttp)
     }
 
     async beforeEachStep(step, flow) {
         await super.beforeEachStep(step, flow)
-        console.log('before', step)
-
+        console.info('before', step)
+        countByStep++
         if (step.selectors) {
-            await page.waitForTimeout(3000)
-            await page.waitForSelector(step.selectors[0], { timeout: 10000 })
+            const selector = step.selectors[step.selectors.length-1]
+            // await page.waitForTimeout(100)
+            try{
+                await page.waitForSelector(selector, { timeout: 5000 })
+                fs.appendFileSync(`${dirCurrent}/result.log`, `OK , ${countByStep}/${currentFlow.steps.length} , ${step.selectors[0]} , ${selector} \n`);
+            }
+            catch(e){
+                fs.appendFileSync(`${dirCurrent}/result.log`, `NG , ${countByStep}/${currentFlow.steps.length} , ${step.selectors[0]} , ${selector} \n`);
+                console.info("/----------------------------------")
+                console.info("--- 例外発生                     ---")
+                console.info("-----------------------------------")
+                console.info("--- Exception")
+                console.error(e)
+                console.info("--- step")
+                console.error(step)
+                for (let index = 0; index < step.selectors.length; index++) {
+                    console.info(`--- step.selectors[${index}]`)
+                    console.error(step.selectors[index])
+                }
+                console.info("--- flow")
+                console.error(flow)
+                for (let index = 0; index < flow.steps.length; index++) {
+                    console.info(`--- flow.steps[${index}]`)
+                    console.error(flow.steps[index])
+                }
+                console.info("-----------------------------------")
+                console.info("----------------------------------/")
+                fs.appendFileSync(`${dirCurrent}/error.json`, JSON.stringify(e));
+            }
+        }else{
+            fs.appendFileSync(`${dirCurrent}/result.log`, `OK , ${countByStep}/${currentFlow.steps.length} ,\n`);
         }
     }
 
     async afterEachStep(step, flow) {
         await super.afterEachStep(step, flow)
-        console.log('after', step)
+        console.info('after', step)
 
         if(0 < countByStep){
             const fullName = `${dirNameByScreenshot}/${countByStep}.png`
             await page.screenshot({path: fullName}); 
         }
-        countByStep++
     }
 
     async afterAllSteps(flow) {
         await super.afterAllSteps(flow)
-        console.log('done')
+        console.info('done',flow)
     }
 }
 
@@ -110,7 +176,17 @@ export async function run(extension) {
         const jsonText = fs.readFileSync(`${dirNameByRecordings}/${recording}`, 'utf8')
         const parsed = parse(JSON.parse(jsonText))
         const runner = await createRunner(parsed, extension)
-        await runner.run()
+        try{
+            await runner.run()
+        } catch(e) {
+            console.info("/----------------------------------")
+            console.info("--- 例外発生                     ---")
+            console.info("-----------------------------------")
+            console.info("--- Exception")
+            console.error(e)
+            console.info("----------------------------------/")
+            fs.appendFileSync(`${dirNameByHttp}/${countByResponse}.error.txt`, JSON.stringify(e));
+        }
     }
 }
 
